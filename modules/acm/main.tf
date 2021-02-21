1cb86364c5aa0597aa82beb0a1c1e0531c95a9e8
sgaba@globallink.com <sgaba@globallink.com>
2021-02-21 19:11:02 +0000

    feat(aws): fix the path to certs [SEAH-150]

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "web_cert" {
-  private_key=file("${path.module}/../${var.environment}/certs/private.key")
-  certificate_body = file("${path.module}/../${var.environment}/certs/main.crt")
-  certificate_chain=file("${path.module}/../${var.environment}/certs/ca.crt")
+  private_key=file("${path.module}/../../${var.environment}/certs/private.key")
+  certificate_body = file("${path.module}/../../${var.environment}/certs/main.crt")
+  certificate_chain=file("${path.module}/../../${var.environment}/certs/ca.crt")
 }
