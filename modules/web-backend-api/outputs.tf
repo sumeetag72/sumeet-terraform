f12c4241c05546eb8e278d5bdb0d5fe48e16e362
sgaba <sgaba@globallink.com>
2021-03-11 19:32:15 +0000

    feat(aws infra): enable domain for fsbl backend api[SEAH-140]

--- modules/web-backend-api/outputs.tf
+++ modules/web-backend-api/outputs.tf
@@ -2,3 +2,8 @@ output "domain_name" {
   description = "default cloud front domain name for web backend api"
   value       = aws_api_gateway_domain_name.web_api_domain_name.cloudfront_domain_name
 }
+
+output "zone_id" {
+  description = "hosted zone id for web backend api"
+  value       = aws_api_gateway_domain_name.web_api_domain_name.cloudfront_zone_id
+}
