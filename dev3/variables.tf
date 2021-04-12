09df24818a5d791373e8a8e1ea5a0e48d6fd6acc
sgaba <sgaba@globallink.com>
2021-04-12 20:04:09 +0100

    feat(dev3 stack): add terraform configs for dev3 [GLUAT-9151]

---
+++ dev3/variables.tf
@@ -0,0 +1,96 @@
+variable "aws_account_id" {
+  type    = string
+  default = null
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "profile" {
+  type    = string
+  default = null
+}
+
+variable "environment" {
+  type    = string
+  default = null
+}
+
+variable "project" {
+  type    = string
+  default = null
+}
+
+variable "dynamo_admin_table_name" {
+  type    = string
+  default = null
+}
+
+variable "register_component_lambda_name" {
+  type    = string
+  default = null
+}
+
+variable "get_components_lambda_name" {
+  type    = string
+  default = null
+}
+
+variable "delete_component_lambda_name" {
+  type    = string
+  default = null
+}
+
+variable "idp-name" {
+  type    = string
+  default = null
+}
+
+variable "user-pool-client-redirect-urls" {
+  type    = list(string)
+}
+
+variable "user-pool-client-logout-urls" {
+  type    = list(string)
+}
+
+variable "deploy_auth" {
+  description = "If set to true, cognito auth will be configured"
+  type        = bool
+  default     = false
+}
+
+variable "user_pool_arn" {
+  description = "needed when deploy auth is disabled"
+  type        = string
+  default = null
+}
+
+variable "whitelisted_ips" {
+  description = "needed when deploy auth is disabled"
+  type        = list
+  default = null
+}
+
+variable "domain_name" {
+  type    = string
+  default = null
+}
+
+variable "acm_certificate_arn" {
+  type    = string
+  default = null
+}
+
+variable "hosted_zone_id" {
+  type    = string
+  default = null
+}
+
+variable "configure_route53" {
+  description = "If set to true, route 53 mappings will be created"
+  type        = bool
+  default     = false
+}
