979a4392ac76387b58c13f87a080b69480f1513e
sgaba <sgaba@globallink.com>
2021-05-21 13:48:04 +0100

    feat(iam): add variables and values for dev1 [SEAH-337]

--- dev1/variables.tf
+++ dev1/variables.tf
@@ -100,3 +100,33 @@ variable "configure_route53" {
   default     = false
 }
 
+variable "admin_api_id" {
+  type    = string
+  default = null
+}
+
+variable "seahorse_admin_group_id" {
+  type    = string
+  default = null
+}
+
+variable "ssa_group_id" {
+  type    = string
+  default = null
+}
+
+variable "bestx_group_id" {
+  type    = string
+  default = null
+}
+
+variable "tradenexus_group_id" {
+  type    = string
+  default = null
+}
+
+variable "fxconnect_group_id" {
+  type    = string
+  default = null
+}
+
