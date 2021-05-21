a7d392419da38cc9d82c9d38659b2cd95c5a95b7
sgaba <sgaba@globallink.com>
2021-05-21 13:48:55 +0100

    feat(iam): add variables and values for dev3 [SEAH-337]

--- dev3/variables.tf
+++ dev3/variables.tf
@@ -147,3 +147,33 @@ variable "delete_preference_lambda_name" {
   type        = string
   default     = null
 }
+
+variable "admin_api_id" {
+  type    = string
+  default = null
+}
+
+variable "seahorse_admin_group_id" {
+  type    = string
+  default = null
+}
+
+variable "ssa_group_id" {
+  type    = string
+  default = null
+}
+
+variable "bestx_group_id" {
+  type    = string
+  default = null
+}
+
+variable "tradenexus_group_id" {
+  type    = string
+  default = null
+}
+
+variable "fxconnect_group_id" {
+  type    = string
+  default = null
+}
